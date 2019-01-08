197c173aa398bf565ccaa5520c4d4f2e2ac570d1
mattkeperling <mattkeperling@gmail.com>
2019-01-08 08:25:39 -0500

    Update URTracker.js

--- URTracker.js
+++ URTracker.js
@@ -1,37 +1,56 @@
-//UR Tracker by pr0f (and kinda phuz)
-function URTracker_bootstrap()
-{
-    var bGreasemonkeyServiceDefined     = false;
+// ==UserScript==
+// @name         UR Tracking
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  ur tracker
+// @author       me
+// @license         GNU GPLv3
+// @include       https://*.waze.com/en-US/editor*
+// @include         /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @grant        none
+// @require https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js?version=229392
+// ==/UserScript==
 
-    try
-    {
-        if ("object" === typeof Components.interfaces.gmIGreasemonkeyService)
-        {
-            bGreasemonkeyServiceDefined = true;
-        }
-    }
-    catch (err)
-    {
-        //Ignore.
-    }
-    if ( "undefined" === typeof unsafeWindow  ||  ! bGreasemonkeyServiceDefined)
-    {
-        unsafeWindow    = ( function ()
-        {
-            var dummyElem   = document.createElement('p');
-            dummyElem.setAttribute ('onclick', 'return window;');
-            return dummyElem.onclick ();
-        } ) ();
-    }
-    /* begin running the code! */
-    coolscript_init();
-}
+/* global W */
+/* global GM_info */
+/* global toastr */
 
-function URTracker_init()
-{
-    showmethemoney();
-}
+(function() {
+    'use strict';
 
-// [...]
-// then at the end of your script, call the bootstrap to get things started
-URTracker_bootstrap();
+    function bootstrap(tries = 1) {
+        if (W && W.map &&
+            W.model && W.loginManager.user &&
+            $ ) {
+            init();
+        } else if (tries < 1000)
+            setTimeout(function () {bootstrap(tries++);}, 200);
+    }
+
+    function init()
+    {
+    var $section = $("<div>");
+        $section.html([
+            '<div>',
+            '<h4>UR Tracker Settings</h4>',
+            '<br>',
+            '<label for="URTrackerAPIkey">API Key</label> <input type="textbox" id="URTrackerAPIkey" class="URTrackerSettingsCheckbox" size=30>',
+            '<hr>',
+            '<div align=right>',
+            'URs Open: <span id="urtrackerOpenURs"></span></br>',
+            'URs @ 4 days: <span id="urTrackerURs4"></span></br>',
+            'URs @ 8 days: <span id="urTrackerURs8"></span></br>',
+            '</div>',
+            '</div>'
+        ].join(' '));
+
+        new WazeWrap.Interface.Tab('UR Tracker', $section.html(), initializeSettings);
+    }
+    function initializeSettings()
+    {
+        //$('#urtrackerOpenURs').text();
+        //$('#urTrackerURs4').text();
+        //$('#urTrackerURs8').text();
+    }
+    bootstrap();
+})();
